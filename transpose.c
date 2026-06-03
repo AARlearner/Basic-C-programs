d57cb502f4b3c27aa68cf784a96002406517cd1f
AARlearner <abdullahalrafi57890@gmail.com>
2026-06-03 20:49:16 +0600

    Add files via upload

---
+++ transpose.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+   int r;
+   printf("Enter the row\n");
+   scanf("%d",&r);
+   int c;
+   printf("Enter the column\n");
+   scanf("%d",&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+          scanf("%d",&arr[i][j]);
+        }
+      
+    }
+    printf("\n");
+     for(int i=0;i<c;i++){
+        for(int j=0;j<r;j++){
+           printf("%d ",arr[j][i]);
+        }
+  printf("\n");
+}
+}
